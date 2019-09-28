c69f993f86290276ee3f228599c9cd8ba3e85941
Michail Roditis <ylovits@gmail.com>
2019-09-28 19:20:39 +0200

    Updates

---
+++ precache-manifest.29fc0ad643f1a70e31f6e749aa0c3ba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6781a9a9ffb01766ed272cfdd6b7c527",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "006a6925e9b08c02d4ba",
+    "url": "/monsters-rolodex/static/css/main.fba35bf7.chunk.css"
+  },
+  {
+    "revision": "6b610ca0911a0dcf307b",
+    "url": "/monsters-rolodex/static/js/2.84fd0de3.chunk.js"
+  },
+  {
+    "revision": "006a6925e9b08c02d4ba",
+    "url": "/monsters-rolodex/static/js/main.149034fc.chunk.js"
+  },
+  {
+    "revision": "0d0f416e7b5fe29edb74",
+    "url": "/monsters-rolodex/static/js/runtime-main.9ce5b26a.js"
+  }
+]);
